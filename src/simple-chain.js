cddd6279a566d0176326536aa3bb5b79a948012d
Nataliia Cherkes <69033364+chernataly2020@users.noreply.github.com>
2020-10-30 19:41:11 +0200

    feat: Solve task chainMaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  result: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.result.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result.push('( ' + value + ' )');
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof position === 'number' && position <= this.getLength() && position > 0 )
+      this.result.splice(position - 1, 1);
+    else {
+      this.result = [];
+      throw new Error();
+    }
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.result.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let ready = this.result;
+    this.result = [];
+    return ready.join('~~');
   }
 };
 
+
 module.exports = chainMaker;
+
