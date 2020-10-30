b5c7920db8822f273d4bdd8ae976f64b9c040b50
Nataliia Cherkes <69033364+chernataly2020@users.noreply.github.com>
2020-10-30 19:38:50 +0200

    Solve task VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
+  constructor(Direct) { this.Direct = (Direct === false);}
+  encrypt(message, key) {
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    if (!message || !key) throw new Error;
+    let displace = 0;
+    let result = "";
+    key = key.repeat( Math.ceil ( message.length / key.length ));
+
+    for (let i = 0; i < message.length; i++) {
+      const code = this.getChar(message[i], key[i - displace], '+');
+      if ( !/[a-z]/ig.test(code)) displace++;
+      result += code;
+    }
+    if (this.Direct === true) result = [...result].reverse().join('');
+    return result;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
+  decrypt(message, key) {
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    if (!message || !key) throw new Error;
+    let displace = 0;
+    let result = "";
+    key = key.repeat( Math.ceil ( message.length / key.length ));
+
+    for (let i = 0; i < message.length; i++) {
+      const code = this.getChar(message[i], key[i - displace]);
+      if ( !/[a-z]/ig.test(code) ) displace++;
+      result += code;
+    }
+    if (this.Direct === true) result = [...result].reverse().join('');
+    return result;
+    }
+
+    getChar(mes, ke, operator) {
+      const messageCode = mes.toUpperCase().codePointAt(0) - 65;
+      const keyCode = ke.toUpperCase().codePointAt(0) - 65;
+      if (messageCode < 0 || messageCode > 25) return mes.toUpperCase();
+
+      let code;
+      if (operator === '+') code = messageCode + keyCode;
+      else code = messageCode - keyCode;
+
+      if (code > 25) code = code - 26;
+      else if (code < 0) code = code + 26;
+      return String.fromCodePoint(code + 65);
   }
 }
-
 module.exports = VigenereCipheringMachine;
